39cc8798b0e27dd00d8c60fd7354898dd62acf39
liujifei <liujifeil@126.com>
2018-05-25 15:46:35 +0800

    CyclicBarrier练习

---
+++ src/current/CyclicBarrierTest.java
@@ -0,0 +1,46 @@
+package current;
+
+import java.util.Random;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+
+public class CyclicBarrierTest {
+    private static ExecutorService threadPool = Executors.newFixedThreadPool(4);
+    public static void main(String[] args) {
+        int threadNum = 4;
+        CyclicBarrier barrier = new CyclicBarrier(threadNum);
+        for(int i = 0; i < threadNum; i++){
+            threadPool.submit(new WorkThread(barrier));
+        }
+    }
+
+    public static class WorkThread implements Runnable{
+
+        private CyclicBarrier barrier;
+        public WorkThread(CyclicBarrier b){
+            this.barrier = b;
+        }
+        @Override
+        public void run() {
+            try {
+                System.out.println(Thread.currentThread().getName() + " 开始执行阶段1");
+                Thread.sleep(Math.abs(new Random().nextInt())%10000);
+                System.out.println(Thread.currentThread().getName() + " 阶段1执行完毕");
+                barrier.await(10000, TimeUnit.MILLISECONDS);
+                System.out.println(Thread.currentThread().getName() + " 开始执行阶段2");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (BrokenBarrierException e) {
+                e.printStackTrace();
+            } catch (TimeoutException e) {
+                e.printStackTrace();
+            }
+        }
+        
+    }
+}
